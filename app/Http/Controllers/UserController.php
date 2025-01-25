42e9cebf40b8d1fd6b4239f2c11a60ec45e111da
Ziyadk147 <Ziyadkhan2333@gmail.com>
2025-01-25 18:08:38 +0500

    changed response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,11 +16,11 @@ class UserController extends Controller
         ]);
         $user = User::where('email' , $request->email)->first();
         if(!$user ||!Hash::check($request->password , $user->password)){
-            return response()->json(['error' => 'Invalid Credentials'], 401);
+            return response()->json([ 'message' => 'Invalid Credentials'], 401);
         }
         else{
             $token = $user->createToken('loginToken')->plainTextToken;
-            return response()->json(['token' => $token , 'status' => 200], 200);
+            return response()->json(['data' => $token, 'message' => 'User Logged In Succesfully','status' => 200], 200);
         }
     }
 
@@ -29,10 +29,10 @@ class UserController extends Controller
         if($request->user()){
 
             $request->user()->tokens()->delete();
-            return response()->json(['message' => 'Logged out'] , 200);
+            return response()->json(['message' => 'Logged out' , 'status' =>200] , 200);
         }
         else{
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized' , 'status' => 401], 401);
         }
 
 
